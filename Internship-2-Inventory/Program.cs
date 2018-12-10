0afad37259cec5ebb50c7254619b7189dbdd4e61
m-luketin <ml46019@oss.unist.hr>
2018-12-10 23:57:31 +0100

    Starting.

---
+++ Internship-2-Inventory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Internship_2_Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
